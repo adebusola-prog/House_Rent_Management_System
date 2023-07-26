8702481149a9636a962a50e929dbea1236436f50
zeu <aadeyeye@afexnigeria.com>
2023-07-26 10:43:27 +0100

    added login

--- house_rent_app/admin.py
+++ house_rent_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Location
+from .models import Location, Picture, ListingAlbum, Listing
 
 admin.site.register(Location)
+admin.site.register(Picture)
+admin.site.register(ListingAlbum)
+admin.site.register(Listing)
--- house_rent_app/models.py
+++ house_rent_app/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 from accounts.models import HouseOwner
 
+
 class Location(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
+
+class Picture(models.Model):
+    image = models.ImageField(upload_to="house/owner")
+
+
 class Listing(models.Model):
     house_owner = models.ForeignKey(HouseOwner, on_delete=models.SET_NULL, null=True)
-    house_pictures = models.ImageField(upload_to= "house/owner", default="avatar.svg")
+    house_picture = models.ImageField(upload_to= "house/owner", default="avatar.svg")
     house_description = models.TextField()
-    location =  models.ForeignKey('Location',on_delete=models.SET_NULL, null=True)
-    
+    location =  models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
+
+
+class ListingAlbum(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    pictures = models.ManyToManyField(Picture)
+
+
